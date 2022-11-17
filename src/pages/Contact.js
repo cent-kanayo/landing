703c40aa328245b565661a2d1c0332a124ecf9e8
cent kanayo <centkanayo@gmail.com>
2022-11-17 09:38:43 +0000

    Adding new icons

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet';
 import reach from '../Assets/reach.svg';
+import { HiOutlineMail } from 'react-icons/hi';
+import { MdCall } from 'react-icons/md';
 const Contact = () => {
   return (
     <main className="view">
@@ -14,10 +16,17 @@ const Contact = () => {
             Reach out to us
           </h1>
           <button className="w-[345px] md:w-[552px] flex justify-center items-center gap-5 mb-8 text-xl bg-bgColor-400 text-white py-2 rounded-full hover:bg-bgColor-100 hover:text-bgColor-400 border border-bgColor-400 transition-all duration-300 ease-linear">
-            <span></span>Send us an email
+            <span>
+              {' '}
+              <HiOutlineMail />
+            </span>
+            Send us an email
           </button>
           <button className="w-[345px] md:w-[552px] flex justify-center items-center gap-5 mb-20 text-xl bg-white text-bgColor-400 py-2 rounded-full hover:bg-bgColor-400 hover:text-white border border-bgColor-400 transition-all duration-300 ease-linear">
-            <span></span>Send us an email
+            <span>
+              <MdCall />
+            </span>
+            Send us an email
           </button>
         </div>
       </div>
